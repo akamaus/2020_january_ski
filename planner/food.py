3f38580a841475788c0721dc721ce40c22a214f5
Dmitry Vyal <dmitryvyal@gmail.com>
2019-12-22 15:12:19 +0300

    [PLANNER] отрефакторен обсчет продуктов и построения списка участников

--- planner/food.py
+++ planner/food.py
@@ -1,94 +1,102 @@
 from members import *
 
+from .person import Person
+
 import numpy as np
 import pandas as pd
 import datetime
 from collections import Counter
 
 
-def food_planning(food, NBreakfast, NDinner, NLunch, NSnack, WomanFoodMaxWeight):
+def food_planning(food, NBreakfast, NDinner, NLunch, NSnack):
     names_counter = Counter()
-    total_weight = 0.0
+    total_weight = 0
 
     for m in Members.values():
-        m.food = []
+        m.food.clear()
 
     tbl = []
 
     for row in food:
+        pname = row[0]
         one_portion_weight = float(row[1])
         portion_weight = one_portion_weight * NMembers
         times = float(row[2])
         weight = portion_weight * times
-
         total_weight += weight
 
         assert len(row) == 3 or len(row) == 4
-        names_counter[row[0]] += 1
-        assert names_counter[row[0]] <= 1
+        names_counter[pname] += 1
+        assert names_counter[pname] <= 1, 'Repeating product %s' % pname
 
-
-
-        if len(row) == 3:
-            tbl.append({'name': row[0],
+        if len(row) == 3 or row[3] is None:
+            tbl.append({'product': pname,
                     'portion': portion_weight,
                     'times': times,
                     'weight': weight})
 
         else:
-            if isinstance(row[3], Member):
+            if isinstance(row[3], P.Person):
                 member = row[3]
                 assert member.name in MembersList
                 member.food.append({'kind' : 'food', 'packing_weight': portion_weight,
-                                    'weight' : weight, 'name' : row[0]})
+                                    'weight' : weight, 'name' : pname})
 
-                tbl.append({'name': row[0],
+                tbl.append(
+                    {'product': pname,
                     'portion': portion_weight,
                     'times': times,
                     'weight': weight, 'Кто несет': member.name})
             else:
                 members, ks = row[3]
-                assert np.sum(ks) - times < 1e-9, print(np.sum(ks), times)
+                assert np.sum(ks) - times < 1e-9, f"more pieces than feeding times for {row}"
                 assert len(members) == len(ks)
                 assert len(members) > 1
 
                 for member, k in zip(members, ks):
                     assert member.name in MembersList
                     member.food.append({'kind' : 'food', 'packing_weight': portion_weight,
-                                        'weight' : portion_weight * k, 'name' : row[0]})
+                                        'weight' : portion_weight * k, 'name' : pname})
 
-                    tbl.append({'name': row[0],
+                    tbl.append({'product': pname,
                                 'portion': portion_weight,
                                 'times': k,
                                 'weight': portion_weight * k, 'Кто несет': member.name})
 
                 if np.sum(ks) < times - 1e-9:
                     # Есть нераспределенное снаряжение
-                    tbl.append({'name': row[0],
+                    tbl.append({'product': pname,
                                 'portion': portion_weight,
                                 'times': times - np.sum(ks),
                                 'weight': portion_weight * (times - np.sum(ks))})
 
+    cur_weight = np.sum([m.food_weight() for m in Members.values()])
 
-    print("Общий вес: {}".format(total_weight))
+    avg_man_weight = np.mean([m.food_weight() for m in Members.values() if m.male])
+    avg_woman_weight = np.mean([m.food_weight() for m in Members.values() if not m.male])
 
-    avg = (total_weight - WomanFoodMaxWeight * NWomans) / (NMembers - NWomans)
-    print("Средний вес на мужчину: {}".format(avg))
-    print("Средний вес на женщину: {}".format(WomanFoodMaxWeight))
-    print("Вес на человека в день: {}\n".format(total_weight / NMembers / ((NBreakfast + NDinner + NSnack + NLunch) / 4)))
+    print("Общий вес: %f" % total_weight)
+    print("Распределенный вес: %f" % cur_weight)
 
-    cur_weight = 0.0
+    print("Средний вес на мужчину: %.1f" % avg_man_weight)
+    print("Средний вес на женщину: %.1f" % avg_woman_weight)
+    print("Вес на человека в день: %.1f" % (total_weight / NMembers / ((NBreakfast + NDinner + NSnack + NLunch) / 4)))
+
+    overweight = {}
     for name, member in sorted(Members.items()):
-        w = np.sum([r['weight'] for r in member.food])
-        cur_weight += w
-        if name in Womans:
-            d = WomanFoodMaxWeight - w
+        w = member.food_weight()
+        if member.male:
+            d = w - avg_man_weight
         else:
-            d = avg - w
+            d = w - avg_woman_weight
+        overweight[name] = d
 
-        print("{}: {}".format(name, d))
+    worst_overweights = sorted(overweight.items(), key=lambda x:abs(x[1]), reverse=True)
+    print('\nПеревес:')
+    for n, d in worst_overweights:
+        print("%s %.0f" % (n, d))
 
-    print("\nОстаток: {}".format(total_weight - cur_weight))
+    print("\nОстаток: %.1f" % (total_weight - cur_weight))
 
     table = pd.DataFrame(data=tbl)
     return table
