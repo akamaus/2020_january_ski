be61b13abceb1f285900b9a54190cc59b4db994f
Dmitry Vyal <dmitryvyal@gmail.com>
2019-12-22 12:46:29 +0300

    imported from lebastr's autumn hiking

---
+++ members.py
@@ -0,0 +1,38 @@
+class Member(object):
+    def __init__(self, name):
+        self.name = name
+        self.equipment = []
+        self.food = []
+        
+    def __repr__(self):
+        return "<Person: name: {}>".format(self.name)
+
+Womans = set(['Бубнова',
+             'Виноградова',
+             'Ткачева'])
+Males = set(['Авраменко',
+             'Герасимов',
+             'Дятлов',
+             'Еналеев',
+             'Лебедев',
+             'Малышев'])
+
+WomanShareEqMaxWeight = 1000
+
+assert len(Womans.intersection(Males)) == 0
+
+MembersList = Womans.union(Males)
+
+NMembers = len(MembersList)
+NWomans = len(Womans)
+
+Members = {}
+for m in MembersList:
+    s = "{} = Member('{}'); Members['{}'] = {}".format(m, m, m, m)
+    exec(s)
+
+print(sorted(MembersList))
+    
+print("Количество участников: {}".format(NMembers))
+print("Количество женщин: {}".format(NWomans))
+print("Сколько максимум несет девушка общака: {}".format(WomanShareEqMaxWeight))
